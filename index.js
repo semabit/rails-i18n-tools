8c5ec61f9f074c7307b0b552d7161556169a0978
Shane Hofstetter <shane.hofstetter@gmail.com>
2019-01-15 19:43:55 +0100

    init

---
+++ index.js
@@ -0,0 +1,4 @@
+export const i18nArAttr = (key) => `activerecord.attributes.${key}`;
+export const i18nArModel = (key) => `activerecord.models.${key}`;
+export const i18nValue = (key) => `values.${key}`;
+export const i18nBool = (value) => value ? `values.boolean.true` : `values.boolean.false`;
