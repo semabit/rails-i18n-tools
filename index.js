2c2c74271862fe8e5989f7d410b63303994d264a
Shane Hofstetter <shane.hofstetter@gmail.com>
2019-01-16 09:29:30 +0100

    add toSnakeCase helper method

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
+// key building
 export const i18nArAttr = (key) => `activerecord.attributes.${key}`;
 export const i18nArModel = (key) => `activerecord.models.${key}`;
 export const i18nValue = (key) => `values.${key}`;
 export const i18nBool = (value) => value ? `values.boolean.true` : `values.boolean.false`;
+
+// other helpers
+export const toSnakeCase = (str) => {
+  return str.split(/(?=[A-Z])/).join('_').toLowerCase();
+};
